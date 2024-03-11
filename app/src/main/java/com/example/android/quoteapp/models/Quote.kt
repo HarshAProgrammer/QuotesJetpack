91286cc87d64fa2fdf3fccdebf08e85773eb1c47
HarshAProgrammer <39998012+HarshAProgrammer@users.noreply.github.com>
2024-03-12 04:03:47 +0530

    first commit

---
+++ app/src/main/java/com/example/android/quoteapp/models/Quote.kt
@@ -0,0 +1,4 @@
+package com.example.android.quoteapp.models
+
+data class Quote(val text:String,val author:String) {
+}
---
+++ app/src/main/java/com/example/android/quoteapp/screens/QuoteDetail.kt
@@ -0,0 +1,65 @@
+package com.example.android.quoteapp.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FormatQuote
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import com.example.android.quoteapp.R
+
+@Composable
+fun QuoteDetail() {
+    Box(
+        contentAlignment = Alignment.Center
+        ,modifier = Modifier
+            .fillMaxSize(1f)
+            .background(
+                Brush.sweepGradient(
+                    colors = listOf(
+                        Color(0xFFffffff),
+                        Color(0xFFE3E3E3)
+                    )
+                )
+            )
+    ){
+        Card(modifier = Modifier.padding(32.dp)) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(16.dp, 24.dp)
+            ) {
+                Image(imageVector = Icons.Filled.FormatQuote, contentDescription = "Quote",
+                    modifier = Modifier
+                        .size(80.dp)
+                        .rotate(180F))
+                Text(text = "Time is the Most valuable thing a man can spend",
+                    fontFamily = FontFamily(Font(R.font.montseratt_regular)),
+                    style = MaterialTheme.typography.bodyLarge)
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = "Theophrastus",
+                    fontFamily = FontFamily(Font(R.font.montseratt_regular)),
+                    style = MaterialTheme.typography.bodySmall)
+            }
+        }
+    }
+}
